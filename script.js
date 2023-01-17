a223ea3c440982deb7e38726ad9da1816f214c11
Md Rehan Alam <76740248+Rehan018@users.noreply.github.com>
2023-01-17 23:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
+	let reversed = str.toLowerCase().split('').reverse().join('');
+  return reversed === str.toLowerCase();
 
 }
 module.exports = palindrome
